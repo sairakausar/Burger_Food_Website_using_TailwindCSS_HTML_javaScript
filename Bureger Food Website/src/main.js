3484de2d9ec6ef223bbe6803f3a9456a80baca1f
sairakausar <sairakausar3@gmail.com>
2024-02-18 00:39:45 -0800

    navbar na

--- Bureger Food Website/src/main.js
+++ Bureger Food Website/src/main.js
@@ -2,8 +2,16 @@ const menuDivClickbutton = document.getElementById('menuIcone')
 
 const NavbarLinksDiv = document.getElementById('navBarLInks')
 
+const closeIconeDiv = document.getElementById("closeFinish")
+
 menuDivClickbutton.addEventListener("click", () => {
     return (
         NavbarLinksDiv.classList.remove("hidden")
+    )
+})
+
+closeIconeDiv.addEventListener("click", () => {
+    return(
+        NavbarLinksDiv.classList.add("hidden")
     )
 })
